1183c149d9f81e1e73f92f5fe83b7d22f793886e
kitae <kitae@kitaeui-MacBookPro.local>
2023-03-02 17:47:55 +0900

    ADD:1-3.feature/main 완료

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -7,6 +7,7 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/main" element={<Main />} />
         <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
